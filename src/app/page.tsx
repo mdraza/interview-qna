68e493187f70ee3b20fbb3e4bc96a32baa830150
mdraza <raza.uidev@gmail.com>
2025-09-20 17:06:13 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import QnA from "./_components/QnA";
 const page = () => {
   return (
     <div className="max-w-7xl mx-auto my-10">
-      <h1 className="text-3xl text-center mb-10">
+      <h1 className="text-2xl text-center mb-10">
         JavaScript - Interview Questions and Answers
       </h1>
       <QnA />
